98180df39c43bad418d40be72f1f2357af31d90e
canh.nguyen <canh.nguyen@brailava.com>
2024-12-05 15:20:23 +0700

    Initial commit

---
+++ libs/web/src/lib/web/web.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-web',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './web.component.html',
+  styleUrl: './web.component.css',
+})
+export class WebComponent {}
