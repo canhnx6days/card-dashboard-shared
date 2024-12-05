98de7ae1ee59790fb1284db508039514984248dd
canh.nguyen <canh.nguyen@brailava.com>
2024-12-05 16:35:38 +0700

    add web lib

--- apps/car-dashboard/src/app/app.config.ts
+++ apps/car-dashboard/src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+} from '@angular/router';
+
 import { appRoutes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), provideClientHydration(),
   ],
 };
--- apps/car-dashboard/src/app/app.routes.ts
+++ apps/car-dashboard/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('@car-dashboard-shared/web').then((c) => c.WebComponent),
+  },
+];
